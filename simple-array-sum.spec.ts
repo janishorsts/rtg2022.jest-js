d8c37a33ca52a09c8fb0888573ec9c418a26cb70
Jānis Horsts <janis.horsts@gmail.com>
2022-03-04 16:54:01 +0000

    tidy

---
+++ simple-array-sum.spec.ts
@@ -0,0 +1,10 @@
+import { simpleArraySum } from "./simple-array-sum";
+
+describe("simpleArraySum", () => {
+  it("Sample Input", () => {
+    const actual = simpleArraySum([1, 2, 3, 4, 10, 11]);
+    const expected = 31;
+
+    expect(actual).toBe(expected);
+  });
+});
